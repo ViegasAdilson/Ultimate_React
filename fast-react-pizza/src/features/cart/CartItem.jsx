6b86c5021ff0c59538c59d79da2c7afe1423d061
ViegasAdilson <adilsonviegas@hotmail.com>
2024-02-15 20:25:36 +0000

    fast-pizza

---
+++ fast-react-pizza/src/features/cart/CartItem.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+function CartItem({ item }) {
+  // eslint-disable-next-line react/prop-types, no-unused-vars
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
---
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,21 @@
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isloading = navigation.state === "loading";
+  return (
+    <div className="layout">
+      {isloading && <Loader />}
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
